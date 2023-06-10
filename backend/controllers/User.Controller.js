d5c0f97c2eb4ce77f0cb4c19c6d0afcd351dbb43
Osmar Peña <osmarpena@infoutilitygt.com>
2023-06-09 21:31:26 -0600

    inicio api registro

--- backend/controllers/User.Controller.js
+++ backend/controllers/User.Controller.js
@@ -15,3 +15,17 @@ exports.newuser = async function (req,res){
 }
 
 
+
+exports.deleteuser = async function (req,res){
+    try{
+        var user = {
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password
+        };
+        res.status(200).send({status: "success", message: "Usuario borrado", data: user});
+    }catch(e){
+        res.status(400).send({status: "error", message: "Error al crear usuario", data: e});
+    }
+}
+
