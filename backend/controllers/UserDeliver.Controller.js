4da994aedb84434cb3e5a88181c4ec2c250c7500
Osmar Peña <3000346440101@ingenieria.usac.edu.gt>
2023-06-14 17:37:04 -0600

    Se termino API Registro

--- backend/controllers/UserDeliver.Controller.js
+++ backend/controllers/UserDeliver.Controller.js
@@ -1,42 +1,92 @@
 
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3") ;
+const database = require("../managers/databaseManager");
+const querysMySQL = require("../querys/querysMySQL");
 const crypto = require('crypto');
 require('dotenv').config();
 
 const randomImageName = (bytes = 32) => crypto.randomBytes(bytes).toString('hex');
 
-
-
-const bucketName = process.env.BUCKET_NAME
-const bucketRegion = process.env.BUCKET_REGION
-const accessKey = process.env.AWS_ACCESS_KEY_ID
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY
-
 const s3 = new S3Client({
-    accessKeyId: accessKey, 
-    secretAccessKey: secretAccessKey,  
-    region: bucketRegion     
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID, 
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,  
+    region: process.env.BUCKET_REGION     
 });
 
 
 
 exports.newdeliveruser = async function (req,res){
     try{
-        const params = {
-            Bucket: bucketName,
-            Key: randomImageName(),
-            Body: req.file.buffer,
-            ContentType: req.file.mimetype,
-        }
 
-        const command = new PutObjectCommand(params)
+        var currentDate = new Date();
 
-        await s3.send(command)
-        const objectUrl = `https://${bucketName}.s3.amazonaws.com/${params.Key}`;
-        res.status(200).send({status: "success", message: "Repartidor creado", objectUrl: objectUrl});
+        var user = {
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            email: req.body.email,
+            nit: req.body.nit,
+            medio_transporte: req.body.medio_transporte,
+            userid: req.body.userid,
+            descripcion: req.body.descripcion,
+            username: req.body.username,
+            password: req.body.password,
+            tipo_empresa: req.body.tipo_empresa,
+            municipio: req.body.municipio,
+            tipo_licencia: req.body.tipo_licencia
+        };
+
+        //Primero hago la validación que no exista el correo
+        database.query(querysMySQL.list_users_byemail,[user.email],async function(err,result,fields){    
+            if (err)throw err;
+                if (result.length>0){
+                res.status(200).send({msg:"El correo ya está asociado a una cuenta.", valid:false})
+                return;
+            }else{
+                //segundoo hago la validación que no exista el usuario
+                database.query(querysMySQL.list_users_byuser,[user.username],function(err,result,fields){    
+                    if (err)throw err;
+                        if (result.length>0){
+                        res.status(200).send({msg:"El usuario ya está asociado a una cuenta.", valid:false})
+                        return;
+                    }else{
+                        database.query(querysMySQL.ins_sol, [currentDate,user.nombre, user.apellido,user.email,user.nit,user.medio_transporte,user.userid,user.descripcion,user.username,user.password,user.tipo_empresa,user.municipio,user.tipo_licencia,0],async function(err,result,fields){
+                            
+                            if(result.insertId){
+                                const params = {
+                                    Bucket: process.env.BUCKET_NAME,
+                                    Key: randomImageName(),
+                                    Body: req.file.buffer,
+                                    ContentType: req.file.mimetype,
+                                }
+                        
+                                const command = new PutObjectCommand(params)
+                        
+                                await s3.send(command)
+                                const objectUrl = `https://${process.env.BUCKET_NAME}.s3.amazonaws.com/${params.Key}`;
+
+                                //Si todo bien con el S3 guardo la imagen en la base de datos
+                                database.query(querysMySQL.ins_sol_image, [user.descripcion,objectUrl,result.insertId],async function(err,result2,fields){
+                                    if (err)throw err;
+                                    //Si todo pasó correctamente, envio el correo
+                                    
+                                    res.status(200).send({status: "success", message: "Solicitud creada con exito"});
+                                });  
+
+
+
+                            }else{
+                                res.status(200).send({msg:"Hubo un error al crear la solicitud", valid:false})
+                            }
+                        });        
+                        
+                    }
+                });
+            }
+        });       
+        
     }catch(e){
         console.log(e)
-        res.status(400).send({status: "error", message: "Error al crear repartidor", data: e});
+        res.status(400).send({status: "error", message: "Error al crear usuario", data: e});
     }
 }
 
@@ -44,7 +94,7 @@ exports.newdeliveruser = async function (req,res){
 exports.getdeliveruser = async function (req,res){
     try{
         const params = {
-            Bucket: bucketName,
+            Bucket: process.env.BUCKET_NAME,
             Key: randomImageName(),
             Body: req.file.buffer,
             ContentType: req.file.mimetype,
@@ -53,7 +103,7 @@ exports.getdeliveruser = async function (req,res){
         const command = new PutObjectCommand(params)
 
         await s3.send(command)
-        const objectUrl = `https://${bucketName}.s3.amazonaws.com/${params.Key}`;
+        const objectUrl = `https://${process.env.BUCKET_NAME}.s3.amazonaws.com/${params.Key}`;
 
         //Aca irá la parte de guardar en la base de datos
         res.status(200).send({status: "success", message: "Datos del repartidor", objectUrl: objectUrl});
@@ -66,7 +116,7 @@ exports.getdeliveruser = async function (req,res){
 exports.updatedeliveruser = async function (req,res){
     try{
         const params = {
-            Bucket: bucketName,
+            Bucket: process.env.BUCKET_NAME,
             Key: randomImageName(),
             Body: req.file.buffer,
             ContentType: req.file.mimetype,
@@ -75,7 +125,7 @@ exports.updatedeliveruser = async function (req,res){
         const command = new PutObjectCommand(params)
 
         await s3.send(command)
-        const objectUrl = `https://${bucketName}.s3.amazonaws.com/${params.Key}`;
+        const objectUrl = `https://${process.env.BUCKET_NAME}.s3.amazonaws.com/${params.Key}`;
 
         //Aca irá la parte de guardar en la base de datos
         res.status(200).send({status: "success", message: "Repartidor modificado", objectUrl: objectUrl});
@@ -88,7 +138,7 @@ exports.updatedeliveruser = async function (req,res){
 exports.deletedeliveruser = async function (req,res){
     try{
         const params = {
-            Bucket: bucketName,
+            Bucket: process.env.BUCKET_NAME,
             Key: randomImageName(),
             Body: req.file.buffer,
             ContentType: req.file.mimetype,
@@ -97,7 +147,7 @@ exports.deletedeliveruser = async function (req,res){
         const command = new PutObjectCommand(params)
 
         await s3.send(command)
-        const objectUrl = `https://${bucketName}.s3.amazonaws.com/${params.Key}`;
+        const objectUrl = `https://${process.env.BUCKET_NAME}.s3.amazonaws.com/${params.Key}`;
 
         //Aca irá la parte de guardar en la base de datos
         res.status(200).send({status: "success", message: "Repartidor eliminado", objectUrl: objectUrl});
