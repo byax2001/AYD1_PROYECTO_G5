f313d200a4b2fc6a15ad5e96bfdc268e9a6820fe
Vernik22 <3003160030101@ingenieria.usac.edu.gt>
2023-06-18 19:52:29 -0600

    Cambio en querys de reportes y municipios

--- backend/controllers/Address.Controller.js
+++ backend/controllers/Address.Controller.js
@@ -28,13 +28,14 @@ exports.getInfoDep = async function (req, res) {
 
 exports.getInfoMun = async function (req, res) {
     try {
-        database.query(querysMySQL.list_municipios, [], async function (err, result, fields) {
-
+        database.query(querysMySQL.list_municipios, [req.params.id], async function (err, result, fields) {
+            if (err)throw err;
             //Luego de ejecutar todos los querys se validan si fueron exitos
             if (result) {
                 //devuelve los 5 resultados en un JSON
                 res.status(200).send({ status: "success", message: "Estos son los municipios:", data: result});
             } else {
+
                 res.status(200).send({ msg: "Se produjo un error al obtener los municipios.", valid: false })
                 return;
 
@@ -44,6 +45,7 @@ exports.getInfoMun = async function (req, res) {
         });
 
     } catch (e) {
+        
         res.status(400).send({ status: "error", message: "Error al obtener informacion de usuarios", data: e });
     }
 }
